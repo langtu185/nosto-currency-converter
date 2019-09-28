90eb6807b0d61e6465d6f80ab07aed01940dd623
Nguyen Thanh Quang Tu <ntquangtu@gmail.com>
2019-09-28 21:46:45 +0300

    Fix deployment

---
+++ README.md
@@ -0,0 +1,10 @@
+# Overview
+
+This is a simple money currency converter web application. The application has the following features:
+
+* Backend: Using Java Spring Boot and Redis cache to build an api.
+* Frontend: Using Bootstrap and jQuery to build a simple form.
+
+The application receives three input value, source currency and target currency. Then it will return the converted value using [api](https://exchangeratesapi.io/)
+
+The application is deployed using [Heroku](https://www.heroku.com). The application can be tested at [link]()  
--- src/main/java/com/nosto/convertor/config/AppConfig.java
+++ src/main/java/com/nosto/convertor/config/AppConfig.java
@@ -3,16 +3,22 @@ package com.nosto.convertor.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class AppConfig{
+public class AppConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder
-				.errorHandler(new DefaultResponseErrorHandler())
-				.build();
+		return builder.errorHandler(new DefaultResponseErrorHandler()).build();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+		http.csrf().disable();
 	}
 
 }
