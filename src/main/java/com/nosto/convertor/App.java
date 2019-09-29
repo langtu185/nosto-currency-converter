a9d718af227248a6b6236d651bcb9f8abbdab1e4
Nguyen Thanh Quang Tu <ntquangtu@gmail.com>
2019-09-29 21:51:40 +0300

    fix build deploy tomcat

--- src/main/java/com/nosto/convertor/App.java
+++ src/main/java/com/nosto/convertor/App.java
@@ -2,11 +2,12 @@ package com.nosto.convertor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class App {
+public class App extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
